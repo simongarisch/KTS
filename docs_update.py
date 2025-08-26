380327bd17419413e25be158e243a6d03d5494c4
simongarisch <31283453+simongarisch@users.noreply.github.com>
2025-08-26 03:43:30 +0000

    docs

--- docs_update.py
+++ docs_update.py
@@ -1,13 +1,20 @@
 import os
+from collections import namedtuple
+
+
+TextSwap = namedtuple("TextSwap", ["old", "new"])
+
+swaps = list()
+swaps.append(TextSwap("/build/_shared/", "build/_shared/"))
+swaps.append(TextSwap("/build/_assets/", "build/_assets/"))
 
 file_path = os.path.join("docs", "index.html")
-old_string = "/build/_shared/"
-new_string = "build/_shared/"
 
 with open(file_path, "r") as f:
     data = f.read()
 
-data = data.replace(old_string, new_string)
+for swap in swaps:
+    data = data.replace(swap.old, swap.new)
 
 with open(file_path, "w") as f:
     f.write(data)
