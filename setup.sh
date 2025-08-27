1b20767fe31b6f7cf20f07fc058d9099f1c3bb93
simongarisch <31283453+simongarisch@users.noreply.github.com>
2025-08-27 08:05:11 +0000

    modified setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# which distro
+cat /etc/os-release
+
 # running in github codespaces which has Python installed by default
 # ensure that jupyterbook is installed from requirements.txt
 python --version # 3.12.1
